be90b0c3ff502ce0a25ea59bd2f7020b78c9ca8b
computer0101 <deveshpatel7643@gmail.com>
2018-04-17 23:31:27 +0530

    implemented google oauth2.0, signup and login backend logic

--- routes/auth/login.js
+++ routes/auth/login.js
@@ -1,10 +1,38 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const router = express.Router();
+
+const User = require('../../models/users');
 
 router.get('/', function(req, res, next) {
-  res.render('auth/login', { title: 'Login' });
+    res.render('auth/login', { title: 'Login' });
 });
+
 router.post('/', function(req, res, next) {
-        res.send('This is POST');
+    User.findOne({ email: req.body.email }).then((resultDb) => {
+        if (resultDb) {
+            bcrypt.compare(req.body.pass1, resultDb.password).then((result) => {
+                console.log(result);
+                if (result) {
+                    const payload = {
+                        id: resultDb.id,
+                        login: 'true'
+                    }
+                    const token = jwt.sign(payload, process.env.JWT_SECRET);
+                    res.cookie('jwt', token);
+                    res.redirect('app');
+                } else {
+                    res.render('auth/login', { title: 'Login', alertMessage: 'Wrong Password' });
+                }
+            }, (err) => {
+                console.error('something went wrong while comparing passwords.');
+            });
+        } else {
+            res.render('auth/login', { title: 'Login', alertMessage: 'Seems like user does not exists.' });
+        }
+    }, (err) => {
+        console.log('something went wrong while searching user from database.');
     });
+});
 module.exports = router;
--- routes/auth/signup.js
+++ routes/auth/signup.js
@@ -1,10 +1,57 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const validator = require('validator');
+const router = express.Router();
+
+const User = require('../../models/users');
+
+const passExpression = '^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})';
 
 router.get('/', function(req, res, next) {
-  res.render('auth/signup', { title: 'Signup' });
+    res.render('auth/signup', { title: 'Signup' });
 });
+
 router.post('/', function(req, res, next) {
-    res.send('This is POST');
-    });
+    let count = 0;
+    const email = validator.isEmail(req.body.email);
+    const password = validator.matches(req.body.pass1, passExpression);
+
+    User.findOne({ email: req.body.email }).then((result) => {
+        if (result) {
+            res.render('auth/signup', { title: 'Signup', alertMessage: 'user already exists' });
+        } else {
+            if (req.body.fname.length < 3) {
+                res.render('auth/signup', { title: 'Signup', alertMessage: 'Valid first name.' });
+            } else if (req.body.number.length !== 10) {
+                res.render('auth/signup', { title: 'Signup', alertMessage: 'Valid mobile number is required.' });
+            } else if (!email) {
+                res.render('auth/signup', { title: 'Signup', alertMessage: 'Valid email is required.' });
+            } else if (!password) {
+                res.render('auth/signup', { title: 'Signup', alertMessage: 'This expression is required.' });
+            } else if (req.body.pass1 !== req.body.pass2) {
+                res.render('auth/signup', { title: 'Signup', alertMessage: 'Both passwords should match.' });
+            } else {
+                bcrypt.genSalt(10, (err, salt) => {
+                    bcrypt.hash(req.body.pass1, salt, (err, hash) => {
+                        const user = new User({
+                            fname: req.body.fname,
+                            phone: req.body.number,
+                            email: req.body.email,
+                            password: hash
+                        });
+                        user.save().then((result) => {
+                            console.log(result);
+                            res.render('auth/login', { title: 'Login', successMessage: 'Successfully signed up. You can now login.' });
+                        });
+                    }, (err) => {
+                        console.error('something went wrong while hashing password.');
+                    });
+                }, (err) => {
+                    console.error('something went wrong while generating salt.');
+                });
+            }
+        }
+    })
+});
 module.exports = router;
