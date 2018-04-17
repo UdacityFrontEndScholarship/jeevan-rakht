03be3a6a2f8037e996176ae6ce3dc5aa5f6f2ce2
computer0101 <deveshpatel7643@gmail.com>
2018-04-18 00:30:23 +0530

    implemented google oauth2.0, signup and login backend logic

--- routes/auth/signup.js
+++ routes/auth/signup.js
@@ -22,36 +22,38 @@ router.post('/', function(req, res, next) {
             res.render('auth/signup', { title: 'Signup', alertMessage: 'user already exists' });
         } else {
             if (req.body.fname.length < 3) {
-                res.render('auth/signup', { title: 'Signup', alertMessage: 'Valid first name.' });
+                res.render('auth/signup', { title: 'Signup', alertMessage: 'Valid first name is required.' });
             } else if (req.body.number.length !== 10) {
                 res.render('auth/signup', { title: 'Signup', alertMessage: 'Valid mobile number is required.' });
             } else if (!email) {
                 res.render('auth/signup', { title: 'Signup', alertMessage: 'Valid email is required.' });
             } else if (!password) {
-                res.render('auth/signup', { title: 'Signup', alertMessage: 'This expression is required.' });
+                res.render('auth/signup', { title: 'Signup', alertMessage: 'At least on capital letter is required.' });
             } else if (req.body.pass1 !== req.body.pass2) {
                 res.render('auth/signup', { title: 'Signup', alertMessage: 'Both passwords should match.' });
             } else {
-                bcrypt.genSalt(10, (err, salt) => {
-                    bcrypt.hash(req.body.pass1, salt, (err, hash) => {
-                        const user = new User({
-                            fname: req.body.fname,
-                            phone: req.body.number,
-                            email: req.body.email,
-                            password: hash
-                        });
-                        user.save().then((result) => {
-                            console.log(result);
-                            res.render('auth/login', { title: 'Login', successMessage: 'Successfully signed up. You can now login.' });
-                        });
-                    }, (err) => {
-                        console.error('something went wrong while hashing password.');
+                const salt = bcrypt.genSaltSync(10);
+                bcrypt.hash(req.body.pass1, salt, (err, hash) => {
+                    if (err) {
+                        console.error(err);
+                        return;
+                    }
+                    const user = new User({
+                        fname: req.body.fname,
+                        phone: req.body.number,
+                        email: req.body.email,
+                        password: hash
                     });
-                }, (err) => {
-                    console.error('something went wrong while generating salt.');
+                    console.log(user);
+                    user.save().then((result) => {
+                        console.log(result);
+                        res.render('auth/login', { title: 'Login', successMessage: 'Successfully signed up. You can now login.' });
+                    }, (err) => {
+                        console.error('something went wrong while save user data to database.', err);
+                    });
                 });
             }
         }
-    })
+    });
 });
 module.exports = router;
