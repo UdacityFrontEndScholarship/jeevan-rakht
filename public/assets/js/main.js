a5452a87e3ab367df7f1d5363ec142b779f6fae1
Imran <strongsoda2@gmail.com>
2018-04-20 14:49:09 -0700

    Added toggle sidebar hamburger menu. The .. main content gets pushed to right and covered with an overlay and becomes.. inactive , while the sidebar is active, to avoid confusion

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,7 +1,7 @@
 // This is our Main JS file.
 // Please modify as and when required.
 // alert('works');
-let didScroll= false;
+let didScroll = false;
 let lastScrollPosition = 0;
 let delta = 2;
 let nav = $('.navbar');
@@ -54,3 +54,33 @@ function hasScrolled() {
     lastScrollPosition = currentScroll;
 
 }
+
+
+$(document).ready(function () {
+    var trigger = $('.hamburger'),
+        overlay = $('.overlay'),
+        isClosed = false;
+
+    trigger.click(function () {
+        hamburger_cross();
+    });
+
+    function hamburger_cross() {
+
+        if (isClosed == true) {
+            overlay.hide();
+            trigger.removeClass('is-open');
+            trigger.addClass('is-closed');
+            isClosed = false;
+        } else {
+            overlay.show();
+            trigger.removeClass('is-closed');
+            trigger.addClass('is-open');
+            isClosed = true;
+        }
+    }
+
+    $('[data-toggle="offcanvas"]').click(function () {
+        $('#wrapper').toggleClass('toggled');
+    });
+});
