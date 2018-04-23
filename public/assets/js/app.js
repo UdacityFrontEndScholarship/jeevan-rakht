c54517e8238e4fabffbb8b05995894a4a8e7970c
crisner <christie.reni@gmail.com>
2018-04-24 03:45:19 +0530

    Implementation - Locate blood app state
    Fix - arrow size
    
    Fix - add link to back arrow

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,2 +1,16 @@
 // Particles JS Initialization
 particlesJS.load("particles-js", "assets/js/particlesjs-config.json");
+
+// Bootstrap Date-Picker
+$(document).ready(function() {
+    var selectDate = $('#when');
+    var contain = $('.wrapper');
+    var options = {
+        format: 'dd/mm/yyyy',
+        container: contain,
+        todayHighlight: true,
+        autoclose: false
+      };
+
+      selectDate.datepicker(options);
+});
