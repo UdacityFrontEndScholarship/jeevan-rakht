1333d95d076b086059262aef7306bdfff42d7d32
Tripti Saini <triptisaini123@gmail.com>
2018-04-29 11:22:16 +0530

    Added Vertical Scroll List
    Changes made at the end from line 87

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -82,3 +82,42 @@ $(document).ready(function () {
         $('#wrapper').toggleClass('toggled');
     });
 });
+
+
+/* Vertical auto scrolling list */
+
+ScrollRate = 40;
+
+function scrollDiv_init() {
+    DivElmnt = document.getElementById('MyDivName');
+    ReachedMaxScroll = false;
+    
+    DivElmnt.scrollTop = 0;
+    PreviousScrollTop  = 0;
+    
+    ScrollInterval = setInterval('scrollDiv()', ScrollRate);
+}
+
+function scrollDiv() {
+    
+    if (!ReachedMaxScroll) {
+        DivElmnt.scrollTop = PreviousScrollTop;
+        PreviousScrollTop++;
+        
+        ReachedMaxScroll = DivElmnt.scrollTop >= (DivElmnt.scrollHeight-DivElmnt.offsetHeight);
+    }
+    else {
+        ReachedMaxScroll = (DivElmnt.scrollTop == 0)?false:true;
+        DivElmnt.scrollTop = 0;
+        PreviousScrollTop  = 0;
+    }
+}
+
+function pauseDiv() {
+    clearInterval(ScrollInterval);
+}
+
+function resumeDiv() {
+    PreviousScrollTop = DivElmnt.scrollTop;
+    ScrollInterval    = setInterval('scrollDiv()', ScrollRate);
+}
