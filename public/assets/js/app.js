a774537eba945af64e702ac34ce7a1bd21c42951
Akash Gajjar <akashgajjar8@gmail.com>
2018-05-09 16:28:12 +0530

    added client-side verification for valid input in signupform and donate form
    fix leading/trailing whitespace and number input issue in input forms of signup and donate,
    
    added methods for validating length, checking for numbers and validating password, improved code readability by implimenting methods for reused code
    
    solved issues #198 and issue #181, implemented methods to validate form inputs on the client side for signup and donate pages

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -51,3 +51,80 @@ $(".input-donation-date[type=date]").blur(function() {
 $("select.empty").change(function() {
     $(this).removeClass('empty');
 })
+
+//function to validate signup form inputs
+$('#signup-submit').on('click',function (event){
+	//flag to decide whether to submit the form or not
+	let flag = false;
+	var radioButtonCheck = $('form input[type=radio]:checked').val();
+	//trim the inputs for leading and trailing whitespaces
+	var pass = $('#password').val();
+	var veriPass = $('#verify').val();
+	if (radioButtonCheck == 'Individual') {
+		var fname = $('#fname').val();
+		var lname = $('#lname').val();
+		if(checkName('First Name',fname)==false) flag = true;
+		if(checkName('Last Name',lname)==false) flag = true;;
+	}
+	if (radioButtonCheck == 'Non-Individual') {
+		var orgname = $('#orgname').val();
+		var license = $('#license').val();
+		if(checkName('Organization Name',orgname)==false) flag = true;
+		if(checkLength('License Nanme',license)==false) flag = true;
+	}
+	//regular expression to chceck if the password contains uppercase and lowercase letters
+	if(validatePass(pass,veriPass)==false) flag = true;
+	if(flag) event.preventDefault();
+});
+
+//function to check if fname/lname are valid by checking their length and checking if they contain a number
+function checkName(field,val){
+	return checkLength(field,val)&&checkNumber(field,val);
+}
+
+//function to check if the input contains any numbers
+function checkNumber(field,val){
+	let regex = /[0-9]/;
+	if(regex.test(val)){
+		alert(field +' should not contain any numbers!');
+		return false;
+	}else return true;
+}
+
+//function to check if the length of an input if greater than 3 characters
+function checkLength(field,val){
+	val = val.trim();
+	if(val.length<3){
+		alert(field +" must contain minimum 3 characters");
+		return false;
+	}else return true;
+}
+
+//function to validate password, takes the arguments original password, and confirm password
+function validatePass(pass,veriPass){
+	console.log('validating pass');
+	let regex = /^(?=.*[a-z])(?=.*[A-Z])/;
+	if (!regex.test(pass) || pass.length < 6) {
+		alert('Password must contain one capital & one small letter with total six chars minimum');
+		return false;
+	}
+	if (pass !== veriPass) {
+		alert('Passwords do not match!');
+		return false;
+	}
+	return true;
+}
+
+//function to validate city name in the donate form
+$('#donate-submit').on('click',function (event){
+  //flag to decide whether to submit the form or not
+  let flag = false;
+  var city = $('form input[name=bookcity]').val().trim();
+  //regular experssion to check for charecters other than uppercase/lowercase letters and space
+  var regex = /[^a-zA-z\s]/;
+  if (regex.test(city)) {
+	alert('city must contain only alphabets and spaces!');
+	flag = true;
+  }
+  if(flag) event.preventDefault();
+});
