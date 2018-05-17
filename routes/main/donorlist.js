ed0b25c577644bac0f785683a1c4374edf4f97f9
gauravsinghaec <gauravsinghaec@gmail.com>
2018-05-17 07:41:31 +0530

    Implement the locate search filter functionalities.

--- routes/main/donorlist.js
+++ routes/main/donorlist.js
@@ -3,38 +3,28 @@ var router = express.Router();
 var { login_required } = require('../../utils/authValidator');
 var { getDonarCount,getDonarList } = require('../../controllers/userController');
 
-router.get('/', function(req, res, next) {
+router.post('/', function(req, res, next) {
+    let userObj = req.body;
+    if(req.user){
+        userObj.user_id = req.user._id
+    };
     let donorlist = [];
     let param ={'title':'Donor List'};    
-    if(req.user && req.user._id){
-        getDonarList({'user_id':req.user._id}, function(err, result) {
-            if (err) {
-                param.alertMessage = "DB Error:"+err.message;
-                res.render('main/donorlist', param);
-            } else if (result) {
-                if (req.query.next) {
-                    var nextUrl = req.query.next;
-                    res.redirect(nextUrl);
-                } else {
-                    console.log(JSON.stringify(result));
+    getDonarList(userObj, function(err, result) {
+        if (err) {
+            res.send(JSON.stringify({'error':err.message}));
+        } else if (result) {
+            if (req.query.next) {
+                var nextUrl = req.query.next;
+                res.redirect(nextUrl);
+            } else {
+                if(req.user && req.user._id){        
                     res.send(JSON.stringify(result));
-                }                                        
-            }
-        });    
-    }else{
-        getDonarCount({}, function(err, result) {
-            if (err) {
-                res.send(JSON.stringify({'error':err.message}));
-            } else if (result) {
-                if (req.query.next) {
-                    var nextUrl = req.query.next;
-                    res.redirect(nextUrl);
-                } else {
-                    console.log(JSON.stringify({'donor_count':result}));
-                    res.send(JSON.stringify({'donor_count':result}));
-                }                                        
-            }
-        });
-    }    
-    });
+                }else{
+                    res.send(JSON.stringify({'donor_count':result.length}));
+                }    
+            }                                        
+        }
+    });    
+});
 module.exports = router;
